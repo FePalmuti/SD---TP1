2a2862a2a629f4504164d087cef935aca307afe7
Felipe Palmuti <fepalmuti@gmail.com>
2019-08-27 02:24:37 -0300

    Vazao implementada

--- Cliente.py
+++ Cliente.py
@@ -13,6 +13,15 @@ while True:
 	sock.sendto(bytearray("Testando ping", "utf-8"), (ip, porta))
 	sock.recvfrom(1024)
 	tempo_final = datetime.now()
-
 	intervalo_de_tempo = tempo_final - tempo_inicial
-	print("Ping: " + str(intervalo_de_tempo.seconds) + "." + str(intervalo_de_tempo.microseconds).zfill(6))
+
+	#Conversao de datetime para float
+	intervalo_de_tempo = intervalo_de_tempo.microseconds
+	print("Ping: " + str(intervalo_de_tempo) + " microsegundos.")
+	#Conversao de microsegundos para segundos
+	intervalo_de_tempo = intervalo_de_tempo/1000000
+
+	vazao = 1024/intervalo_de_tempo
+	#Conversao de B/s para MB/s
+	vazao = vazao/1048576
+	print("Vazao: " + str(round(vazao, 2)) + " MB/s.\n")
