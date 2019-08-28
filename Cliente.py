9a4740d4423eda850e6d6833980630f1f6a1ef6b
Felipe Palmuti <fepalmuti@gmail.com>
2019-08-28 00:17:42 -0300

    Taxa de perda implementada + calculo das medias

--- Cliente.py
+++ Cliente.py
@@ -3,25 +3,43 @@ from datetime import datetime
 
 ip = "127.0.0.1"
 porta = 7000
+timeout = 200 #Microsegundos
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
+	qnt_pacotes_perdidos = 0
+	somatorio_rtt = 0
+	somatorio_vazao = 0
+
 	input("Digite \"enter\" para pingar.")
 
-	tempo_inicial = datetime.now()
-	sock.sendto(bytearray("Testando ping", "utf-8"), (ip, porta))
-	sock.recvfrom(1024)
-	tempo_final = datetime.now()
-	intervalo_de_tempo = tempo_final - tempo_inicial
+	for i in range(100):
+		tempo_inicial = datetime.now()
+		sock.sendto(bytearray("Testando ping", "UTF-8"), (ip, porta))
+		sock.recvfrom(1024)
+		tempo_final = datetime.now()
+		intervalo_de_tempo = tempo_final - tempo_inicial
 
-	#Conversao de datetime para float
-	intervalo_de_tempo = intervalo_de_tempo.microseconds
-	print("Ping: " + str(intervalo_de_tempo) + " microsegundos.")
-	#Conversao de microsegundos para segundos
-	intervalo_de_tempo = intervalo_de_tempo/1000000
+		#O intervalo de tempo passa a ser em microsegundos
+		intervalo_de_tempo = intervalo_de_tempo.microseconds
+		if(intervalo_de_tempo > timeout):
+			#Contabiliza o pacote perdido
+			qnt_pacotes_perdidos += 1
+		else:
+			#Contabiliza o RTT
+			somatorio_rtt += intervalo_de_tempo
+			#Conversao de microsegundos para segundos
+			intervalo_de_tempo = intervalo_de_tempo/1000000
+			#Calculo da vazao
+			vazao = 1024/intervalo_de_tempo
+			#Conversao de B/s para MB/s
+			vazao = vazao/1048576
+			#Contabiliza a vazao
+			somatorio_vazao += vazao
 
-	vazao = 1024/intervalo_de_tempo
-	#Conversao de B/s para MB/s
-	vazao = vazao/1048576
-	print("Vazao: " + str(round(vazao, 2)) + " MB/s.\n")
+	rtt_medio = somatorio_rtt / (100 - qnt_pacotes_perdidos)
+	print("RTT medio: " + str(int(rtt_medio)) + " microsegundos.")
+	vazao_media = somatorio_vazao / (100 - qnt_pacotes_perdidos)
+	print("Vazao: " + str(round(vazao_media, 2)) + " MB/s.")
+	print("Taxa de perda: " + str(qnt_pacotes_perdidos) + "%\n")
--- Servidor.py
+++ Servidor.py
@@ -8,5 +8,4 @@ sock.bind((ip, porta))
 
 while True:
 	mensagem, endereco = sock.recvfrom(1024)
-	print("Chegou!")
-	sock.sendto(bytearray("Resposta ao teste", "utf-8"), (endereco))
+	sock.sendto(bytearray("Resposta ao teste", "UTF-8"), (endereco))
